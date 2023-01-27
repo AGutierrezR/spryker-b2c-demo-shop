24b62742e9c32cd312c7f3bfeb56b36e86538435
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-01-27 23:57:26 +0100

    Create a transformer for translating an antelope-id
    We want to store the ID of an Antelope in the customer table and for the frontend to display the name instead of the ID, we need to add a data-transformer
    
    This will set up the translation of an id to the name or the name to the id with the help of TrainingClient, which we will inject form the CustomerPageFactory
    
    The AntelopeTransformer class will receive the TrainingClient in the constructor and will have two method, transform and reverseTransform
    
    The transform method, will be triggeris when the form field is initialized with its default data
    
    The reverseTransform This method is called when {@link Form::submit()} is called to transform the requests tainted data into an acceptable format.
    
    In the CustomerPageFactory we create a method createAntelopeTransformer() to make AntelopeTransform available

--- src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
+++ src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
@@ -3,10 +3,19 @@
 namespace Pyz\Yves\CustomerPage;
 
 use Pyz\Client\Training\TrainingClientInterface;
+use Pyz\Yves\CustomerPage\Form\FormFactory;
+use Pyz\Yves\CustomerPage\Form\Transformer\AntelopeTransformer;
 use SprykerShop\Yves\CustomerPage\CustomerPageFactory as SprykerCustomerPageFactory;
 
 class CustomerPageFactory extends SprykerCustomerPageFactory
 {
+    public function createAntelopeTransformer(): AntelopeTransformer
+    {
+        return new AntelopeTransformer(
+            $this->getTrainingClient()
+        );
+    }
+
     public function getTrainingClient(): TrainingClientInterface
     {
         return $this->getProvidedDependency(CustomerPageDependencyProvider::CLIENT_TRAINING);
