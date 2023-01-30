142e49f9c93eda48b6a804604e5f1ea345544e55
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-01-30 19:20:51 +0100

    Update Factory with our business models instantiations
    The EntityManager and the Repository are resolved in the BusinessFactory

--- src/Pyz/Zed/HelloSpryker/Business/HelloSprykerBusinessFactory.php
+++ src/Pyz/Zed/HelloSpryker/Business/HelloSprykerBusinessFactory.php
@@ -2,10 +2,16 @@
 
 namespace Pyz\Zed\HelloSpryker\Business;
 
+use Pyz\Zed\HelloSpryker\Business\Reader\StringReader;
 use Pyz\Zed\HelloSpryker\Business\Reverser\StringReverser;
 use Pyz\Zed\HelloSpryker\Business\Reverser\StringReverserInterface;
+use Pyz\Zed\HelloSpryker\Business\Writer\StringWriter;
 use Spryker\Zed\Kernel\Business\AbstractBusinessFactory;
 
+/**
+ * @method \Pyz\Zed\HelloSpryker\Persistence\HelloSprykerEntityManagerInterface getEntityManager()
+ * @method \Pyz\Zed\HelloSpryker\Persistence\HelloSprykerRepositoryInterface getRepository()
+ */
 class HelloSprykerBusinessFactory extends AbstractBusinessFactory
 {
     /**
@@ -14,5 +20,15 @@ class HelloSprykerBusinessFactory extends AbstractBusinessFactory
     public function createStringReverser(): StringReverserInterface
     {
         return new StringReverser();
+    }
+
+    public function createStringReader()
+    {
+        return new StringReader($this->getRepository());
+    }
+
+    public function createStringWriter()
+    {
+        return new StringWriter($this->getEntityManager());
     }
 }
