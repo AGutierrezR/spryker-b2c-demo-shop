2a3734d768ccfac3c221c49669c4997ed505f9e3
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-01-30 12:28:26 +0100

    Create the IndexController class to send data to the view

---
+++ src/Pyz/Zed/HelloSpryker/Communication/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Pyz\Zed\HelloSpryker\Communication\Controller;
+
+use Spryker\Zed\Kernel\Communication\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+
+class IndexController extends AbstractController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array
+     */
+    public function indexAction(Request $request)
+    {
+        return ['string' => 'Hello Spryker!'];
+    }
+}
