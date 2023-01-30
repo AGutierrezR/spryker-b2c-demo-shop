c730c8f3877fcf5a4a2b6bd90e4d3ffaca19a7cc
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-01-30 12:33:00 +0100

    Create the Facade to expose the model instance to the Controller

---
+++ src/Pyz/Zed/HelloSpryker/Business/HelloSprykerFacade.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pyz\Zed\HelloSpryker\Business;
+
+use Spryker\Zed\Kernel\Business\AbstractFacade;
+
+/**
+ * @method \Pyz\Zed\HelloSpryker\Business\HelloSprykerBusinessFactory getFactory()
+ */
+class HelloSprykerFacade extends AbstractFacade implements HelloSprykerFacadeInterface
+{
+    public function reverseString(string $stringToReverse): string
+    {
+        return $this->getFactory()
+            ->createStringReverser()
+            ->reverseString($stringToReverse);
+    }
+}
