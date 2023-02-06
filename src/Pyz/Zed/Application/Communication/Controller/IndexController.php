b50e7863b468c35e4b401e7035960e2e570d72ce
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-02-06 13:12:31 +0100

    Different Stores, Different Logic

---
+++ src/Pyz/Zed/Application/Communication/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Pyz\Zed\Application\Communication\Controller;
+
+use Spryker\Zed\Application\Communication\Controller\IndexController as SprykerIndexController;
+
+class IndexController extends SprykerIndexController
+{
+    /**
+     * @return string
+     */
+    public function indexAction()
+    {
+        return $this->viewResponse([
+            'message' => 'Hello DE Store!'
+        ]);
+    }
+}
