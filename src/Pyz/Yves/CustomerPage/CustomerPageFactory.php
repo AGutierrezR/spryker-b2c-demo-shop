1d639a8d4b507f8fd070585dfdb6f59f411cff01
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-02-08 15:10:03 +0100

    Hands-on Task: Extending Spryker functionality - Part 4

--- src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
+++ src/Pyz/Yves/CustomerPage/CustomerPageFactory.php
@@ -3,11 +3,17 @@
 namespace Pyz\Yves\CustomerPage;
 
 use Pyz\Client\Training\TrainingClientInterface;
+use Pyz\Yves\CustomerPage\Form\FormFactory;
 use Pyz\Yves\CustomerPage\Form\Transformer\AntelopeTransformer;
 use SprykerShop\Yves\CustomerPage\CustomerPageFactory as SprykerCustomerPageFactory;
 
 class CustomerPageFactory extends SprykerCustomerPageFactory
 {
+    public function createCustomerFormFactory()
+    {
+        return new FormFactory();
+    }
+
     public function createAntelopeTransformer(): AntelopeTransformer
     {
         return new AntelopeTransformer(
