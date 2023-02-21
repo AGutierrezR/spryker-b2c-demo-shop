f36ec17fd4734acebaef1b7349de0aa4ed35088c
Andres Gutierrez Ramirez <agutierrez.codes@gmail.com>
2023-02-21 21:33:31 +0100

    Hands-on Back Office - Part 1

---
+++ src/Pyz/Zed/AntelopeGui/Communication/AntelopeGuiCommunicationFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pyz\Zed\AntelopeGui\Communication;
+
+use Orm\Zed\Antelope\Persistence\PyzAntelopeQuery;
+use Pyz\Zed\AntelopeGui\AntelopeGuiDependencyProvider;
+use Pyz\Zed\AntelopeGui\Communication\Table\AntelopeTable;
+use Spryker\Zed\Kernel\Communication\AbstractCommunicationFactory;
+
+class AntelopeGuiCommunicationFactory extends AbstractCommunicationFactory
+{
+    public function createAntelopeTable()
+    {
+        return new AntelopeTable(
+            $this->getAntelopePropelQuery()
+        );
+    }
+
+    public function getAntelopePropelQuery(): PyzAntelopeQuery
+    {
+        return $this->getProvidedDependency(AntelopeGuiDependencyProvider::PROPEL_QUERY_ANTELOPE);
+    }
+}
